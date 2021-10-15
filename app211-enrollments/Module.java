d8e4cdea77c4e91d701975c0ebdec95e0423385e
Karanpreet singh <22041636@bucks.ac.uk>
2021-10-15 13:36:36 +0100

    Edit my name

--- app211-enrollments/Module.java
+++ app211-enrollments/Module.java
@@ -2,8 +2,8 @@
 /**
  * Write a description of class Module here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author Karanpreet Singh
+ * @version 15/10/2021
  */
 public class Module
 {
